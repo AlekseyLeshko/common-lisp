bd128ab926cd5f98c32d62bef6cd0811b09e203b
AlekseyLeshko <alekseyleshko@gmail.com>
2020-05-10 19:26:39 +0800

    Add example with dynamic variables

--- examples/local_variables.lisp
+++ examples/local_variables.lisp
@@ -15,3 +15,18 @@
   y))
 
 (print (let ((x 1)) x))
+
+(print "dynamic variables")
+
+(defparameter *newString* "I'm global")
+(defun print-variable ()
+  (print *newString*))
+
+(print *newString*)
+(print-variable)
+
+(let ((*newString* "I have dynamic extend"))
+  (print-variable))
+
+(print *newString*)
+(print-variable)
