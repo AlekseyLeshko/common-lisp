302b75ca078b4d5f6d1f78f5a6234176f47c343f
AlekseyLeshko <alekseyleshko@gmail.com>
2020-05-13 23:04:48 +0800

    More list in lisp

--- examples/lists.lisp
+++ examples/lists.lisp
@@ -1,3 +1,18 @@
 (print (list 1 2 3))
 (print (first (list 1 2 3)))
 (print (second (list 1 2 3)))
+
+
+(defparameter my-list (list 1 2 3))
+(print my-list)
+
+(setf (second my-list) 7)
+(print my-list)
+
+(print (nth 1 (list 1 2 3)))
+(print (nth 1 my-list))
+
+(setf (nth 1 my-list) 65)
+(print my-list)
+(setf (second my-list) 70)
+(print my-list)
