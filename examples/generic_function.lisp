a61ed4868ad94ed763c29fc6077bbff05f5ab229
AlekseyLeshko <alekseyleshko@gmail.com>
2020-05-14 00:34:25 +0800

    Add another generic method

--- examples/generic_function.lisp
+++ examples/generic_function.lisp
@@ -7,5 +7,9 @@
 (defmethod description ((object float))
   (format nil "The float ~3,3f" object))
 
+(defmethod description ((object string))
+  (format nil "The string ~s" object))
+
 (print (description 10))
 (print (description 3.140))
+(print (description "string"))
